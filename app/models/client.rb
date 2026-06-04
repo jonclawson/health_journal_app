11383e1d43bcdff31592756f7b589e9a600ef9cf
Jonathan Clawson <jonathanclawson@Jonathans-MacBook-Pro.local>
2026-06-04 13:27:42 -0700

    add journal queries

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,7 @@
 class Client < ApplicationRecord
   has_many :memberships, dependent: :destroy
   has_many :providers, through: :memberships
-  has_many :journal_entries, dependent: :destroy
+  has_many :journal_entries, -> { order(created_at: :desc) }, dependent: :destroy
 
   validates :name, :email, presence: true
   validates :email, uniqueness: true
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,7 +1,7 @@
 class Provider < ApplicationRecord
   has_many :memberships, dependent: :destroy
   has_many :clients, through: :memberships
-  has_many :journal_entries, through: :clients
+  has_many :journal_entries, -> { order(created_at: :desc) }, through: :clients
 
   validates :name, :email, presence: true
   validates :email, uniqueness: true
