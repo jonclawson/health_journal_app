29beb372200d6d008c50481691ed5883146e44af
Jonathan Clawson <jonathanclawson@Jonathans-MacBook-Pro.local>
2026-06-04 18:27:55 -0700

    use enum for plan

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,6 +2,7 @@ class Membership < ApplicationRecord
   belongs_to :provider
   belongs_to :client
 
+  enum :plan, { basic: 'basic', premium: 'premium' }, validate: true
   validates :plan, inclusion: { in: %w[basic premium], message: "%{value} is not a valid plan" }
   validates :client_id, uniqueness: { scope: :provider_id, message: "is already signed up with this provider" }
 end
